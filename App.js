d64ca5518414f5e156d784c37ad65b4a434c758c
prajjal-saxena <prajjalsaxena@gmail.com>
2026-03-19 00:25:40 +0530

    episode-4

--- App.js
+++ App.js
@@ -9,24 +9,55 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src="url" />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const RestaurantCard = () => {
    return (
-     <div className="header">
-        <div className="logo-container">
-            
+      <div className="res-card" style={{backgroundColor: "#f0f0f0"}}>
+         <img className="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/kctsaavrl3spve81uhor" />
+         <h3>Meghna Foods</h3>
+         <h4>Biryani, North India, Asia</h4>
+         <h4>4.4 star</h4>
+         <h4>38 minutes</h4>
+      </div>
+   )
+}
+const Body = () => {
+   return (
+      <div className= "body">
+        <div className="Search">Search</div>
+        <div className="res-container">
+           <RestaurantCard />
+           <RestaurantCard />
+
         </div>
-     </div>
+      </div>
    )
 }
 
 const AppLayout = () => {
-    return (
-        <div className="app">
-           <Header />
-           <Body/> 
-        </div>
-    )
-}
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<AppLayout/>)
-
+root.render(<AppLayout />);
